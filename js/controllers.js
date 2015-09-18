252df2d710f17fd9865d79ccbdc3f5215be045ed
KyleRattet <krattet@gmail.com>
2015-09-17 22:04:45 -0600

    updated with angular, replaced jquery

---
+++ js/controllers.js
@@ -0,0 +1,55 @@
+app.controller('mainController', function($scope) {
+  $scope.greeting = "Hello-World!";
+
+
+
+
+
+  $scope.calculate = function () {
+      mealPrice = $scope.mealPrice;
+      taxRate = $scope.taxRate;
+      tipRate = $scope.tipRate;
+
+      $scope.subTotal = mealPrice + (mealPrice*taxRate);
+      $scope.tip = mealPrice*tipRate;
+
+
+      $scope.grandTotal = $scope.subTotal + $scope.tip;
+
+      $scope.mealCount ++;
+      // var total = $scope.totalTips + $scope.tip;
+      $scope.totalTips = (+$scope.totalTips + +$scope.tip);
+
+
+      $scope.averageTip = (+$scope.totalTips/+$scope.mealCount);
+
+      $scope.mealPrice = "";
+      $scope.taxRate = "";
+      $scope.tipRate = "";
+
+
+  };
+
+  $scope.clear = function () {
+    $scope.subTotal = "";
+    $scope.tip = "";
+    $scope.grandTotal= "";
+  };
+
+  $scope.reset = function () {
+    $scope.subTotal = "";
+    $scope.tip = "";
+    $scope.grandTotal= "";
+    $scope.totalTips = "";
+    $scope.mealCount = "";
+    $scope.averageTip = "";
+  };
+
+      $scope.totalTips = "";
+      $scope.mealCount = "";
+      $scope.averageTip = "";
+
+
+
+
+});
